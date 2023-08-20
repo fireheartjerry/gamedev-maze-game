807023a7153ffe062a5fa3a082dd796e2fda30d1
Fireheartjerry <fireheartjerry@gmail.com>
2023-08-20 14:06:20 -0400

    Added game functions

--- main.py
+++ main.py
@@ -14,21 +14,50 @@ FPS_clock = pygame.time.Clock()
 width = 1000
 height = 600
 
-# main function
-def main():
-    pygame.init()
-
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Maze Game!")
-    screen.fill(BLACK)
-
+def title_screen(screen):
+    start_btn = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Start",
+        action=GameState.NEWGAME,
+    )
     quit_btn = UIElement(
-        center_position=(500, 500),
-        font_size=50,
-        bg_rgb=None,
+        center_position=(400, 500),
+        font_size=30,
+        bg_rgb=BLUE,
         text_rgb=WHITE,
         text="Quit",
         action=GameState.QUIT,
+    )
+
+    buttons = [start_btn, quit_btn]
+
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLUE)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+            button.draw(screen)
+
+        pygame.display.flip()
+
+
+def play_level(screen):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Return to main menu",
+        action=GameState.TITLE,
     )
 
     while True:
@@ -36,13 +65,32 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 mouse_up = True
+        screen.fill(BLUE)
 
-        screen.fill(BLACK)
-        ui_action = quit_btn.update(pygame.mouse.get_pos(), mouse_up)
+        ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
         if ui_action is not None:
-            return
-        quit_btn.draw(screen)
+            return ui_action
+        return_btn.draw(screen)
+
         pygame.display.flip()
+
+# main function
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    game_state = GameState.TITLE
+
+    while True:
+        if game_state == GameState.TITLE:
+            game_state = title_screen(screen)
+
+        if game_state == GameState.NEWGAME:
+            game_state = play_level(screen)
+
+        if game_state == GameState.QUIT:
+            pygame.quit()
+            return
 
 if __name__ == "__main__":
     main()
