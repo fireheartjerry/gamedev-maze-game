a5f95e9a954d4e8202b0de01ab53e64de194918b
Fireheartjerry <fireheartjerry@gmail.com>
2023-08-21 09:51:20 -0400

    Added game loop function

--- modules/game.py
+++ modules/game.py
@@ -60,3 +60,24 @@ def title_screen(screen):
             button.draw(screen)
 
         pygame.display.flip()
+
+def game_loop(screen, buttons):
+    """ Handles game loop until an action is return by a button in the
+        buttons sprite renderer.
+    """
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLUE)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+
+        buttons.draw(screen)
+        pygame.display.flip()
