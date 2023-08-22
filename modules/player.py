04675537ba419b63768ac2daee54ee56ea2d0feb
Fireheartjerry <fireheartjerry@gmail.com>
2023-08-22 17:16:26 -0400

    Changed player stats, added pycache files

--- modules/player.py
+++ modules/player.py
@@ -1,10 +1,9 @@
 class Player:
     """ Stores information about a player """
 
-    def __init__(self, score=0, lives=3, level=1):
-        self.score = score
+    def __init__(self, lives=3, level=1, x=0, y=35):
         self.lives = lives
         self.level = level
-        self.x = 0
-        self.y = 35
+        self.x = x
+        self.y = y
         self.hack = False
