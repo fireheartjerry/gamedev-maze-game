9ffca0baa19952c30a226b0a602281cc3939b230
Jerry Li <fireheartjerry@gmail.com>
2023-08-28 09:53:01 -0400

    Update docstrings

--- modules/elements.py
+++ modules/elements.py
@@ -2,7 +2,14 @@ import pygame
 from .constants import RED
 
 class Player:
-    """ Player class, the user-controlled part of the game. """
+    """
+        Player class, the user-controlled part of the game.\n
+        Args:\n
+            `lives` (int, optional): Player lives. Defaults to 3.\n
+            `level` (int, optional): Current level. Defaults to 1.\n
+            `x` (int, optional): x position. Defaults to 0.\n
+            `y` (int, optional): y position. Defaults to 35.\n
+    """
 
     def __init__(self, lives=3, level=1, x=0, y=35):
         self.lives = lives
@@ -16,32 +23,34 @@ class Player:
         self.y = 35
 
 class Wall(pygame.Rect):
-    """A maze wall, another vital aspect of the game
     """
-    def __init__(self, x, y, width, height, colour=RED):
+        A maze wall, another vital aspect of the game.\n
+        Args:\n
+            `x` (int): x position.\n
+            `y` (int): y position.\n
+            `width` (int): width of the wall.\n
+            `height` (int): height of hte wall\n
+            `colour` (tuple(int, int, int), optional): Wall color. Defaults to RED.\n
+    """
+    def __init__(self, x, y, width, height, colour=RED):   
         super().__init__(x, y, width, height)
         self.colour = colour
     
     def set_colour(self, colour):
         """
         Set the colour of the maze wall.
-
-        Args:
-            `colour` (`tuple`): A tuple representing the RGB colour.
         """
         self.colour = colour
 
     def draw(self, surface):
         """
         Draw the maze wall on a given pygame surface.
-
-        Args:
-            `surface` (`pygame.Surface`): The surface to draw the wall on.
         """
         pygame.draw.rect(surface, self.colour, self)
 
 class Maze:
-    """Maze class, main element of game.\n
+    """
+        Maze class, main element of game.\n
         Args:\n
             `walls` (list, optional): A list of the maze walls. Defaults to [].\n
             `start` (tuple, optional): Start position. Defaults to (0, 0).\n
@@ -58,38 +67,24 @@ class Maze:
     def add_wall(self, wall):
         """
         Add a `Wall` object to the maze.
-
-        Args:
-            wall (`Wall`): The MazeWall object to add.
         """
         self.walls.append(wall)
 
     def set_start_pos(self, x, y):
         """
         Set the starting position in the maze.
-
-        Args:
-            `x` (`int`): The x-coordinate of the starting position.\n
-            `y` (`int`): The y-coordinate of the starting position.
         """
         self.start = (x, y)
 
     def set_end_pos(self, x, y):
         """
         Set the ending position in the maze.
-
-        Args:
-            `x` (`int`): The x-coordinate of the ending position.\n
-            `y` (`int`): The y-coordinate of the ending position.
         """
         self.end = (x, y)
 
     def draw(self, surface):
         """
         Draw the maze walls on a given pygame surface.
-
-        Args:
-            `surface` (pygame.Surface): The surface to draw the maze on.
         """
         for wall in self.walls:
             wall.draw(surface)
@@ -97,12 +92,6 @@ class Maze:
     def touching(self, player, hack=False):
         """
         Check if a position (x, y) is valid within the maze.
-
-        Args:
-            `player`: An instance of the player class
-
-        Returns:
-            `bool`: True if the player is not touching a wall, False otherwise.
         """
         if (hack):
             return False
