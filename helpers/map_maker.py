98ef3f2a6b8aaaeec0c244a90e8fe64979b583af
Jerry Li <fireheartjerry@gmail.com>
2023-08-30 11:27:53 -0400

    Updated indents

--- helpers/map_maker.py
+++ helpers/map_maker.py
@@ -8,18 +8,18 @@ def gethex():
 
 # stylesheet
 BTN_STLYLESHEET = {
-    "bg" : "#888888",
-    "fg": "#EEEEEE",
-    "relief" : "flat",
-    "activebackground" : "#000000",
-    "activeforeground": "#FFFFFF",
-    "font" : ("Calibri", 15)
+	"bg" : "#888888",
+	"fg": "#EEEEEE",
+	"relief" : "flat",
+	"activebackground" : "#000000",
+	"activeforeground": "#FFFFFF",
+	"font" : ("Calibri", 15)
 }
 
 LABEL_STYLESHEET = {
-    "bg": "#000000",
-    "fg": "#FFFFFF",
-    "relief" : "flat"
+	"bg": "#000000",
+	"fg": "#FFFFFF",
+	"relief" : "flat"
 }
 
 # gui setup
@@ -45,16 +45,16 @@ colors = {
 }
 
 def startEditor():
-    designButton.destroy()
-    toolLabel.pack()
-    canv.pack()
-    toolFrame.pack()
-    setButton.grid(row=0, column=0, padx=5)
-    delButton.grid(row=0, column=1, padx=5)
-    configMenu[0].grid(row=1, column=0, padx=2, pady = 10)
-    configMenu[1].grid(row=1, column=1, pady = 10)
-    helpButton.grid(row=0, column=2, padx=5)
-    printDataButton.grid(row=0, column=3, padx = 5)
+	designButton.destroy()
+	toolLabel.pack()
+	canv.pack()
+	toolFrame.pack()
+	setButton.grid(row=0, column=0, padx=5)
+	delButton.grid(row=0, column=1, padx=5)
+	configMenu[0].grid(row=1, column=0, padx=2, pady = 10)
+	configMenu[1].grid(row=1, column=1, pady = 10)
+	helpButton.grid(row=0, column=2, padx=5)
+	printDataButton.grid(row=0, column=3, padx = 5)
 
 def placeWall():
 	currentTool[0] = "placewall"
@@ -73,40 +73,40 @@ Press right click or z to undo.
 Press Ctrl-P to print out wall data to console.""")
 
 def delElement():
-    global canv, data, rects
-    if len(rects) == 0:
-        return
-    canv.delete(rects.pop())
-    data.pop()
+	global canv, data, rects
+	if len(rects) == 0:
+		return
+	canv.delete(rects.pop())
+	data.pop()
 
 def onclick(event):
-    global canv, data, rects
-    if currentTool[0] == "placewall":
-        if prevclick[0] is False:
-            prevclick[0] = event.x
-            prevclick[1] = event.y
-            prevclick[2] = canv.create_oval(event.x-3, event.y-3, event.x+3, event.y+3, fill="white")
-        else:
-            name = configMenu[1].get(1.0, "end-1c")
-            col = "#000000"
-            if name in colors:
-                col = colors[name]
-            else:
-                col = gethex()
-                colors[name] = col
-            rects.append(canv.create_rectangle(prevclick[0], prevclick[1], event.x, event.y, fill=col))
-            data.append([min(prevclick[0], event.x), min(prevclick[1], event.y),
-            	max(prevclick[0], event.x) - min(prevclick[0], event.x), max(prevclick[1], event.y) - min(prevclick[1], event.y),
-            	configMenu[1].get(1.0, "end-1c")])
-            prevclick[0] = False
-            canv.delete(prevclick[2])
-    elif currentTool[0] == "deletewall":
-        for i in range(0, len(data)):
-            if 0 < event.x - data[i][0] < data[i][2] and 0 < event.y - data[i][1] < data[i][3]:
-                canv.delete(rects[i])
-                data.pop(i)
-                rects.pop(i)
-                break
+	global canv, data, rects
+	if currentTool[0] == "placewall":
+		if prevclick[0] is False:
+			prevclick[0] = event.x
+			prevclick[1] = event.y
+			prevclick[2] = canv.create_oval(event.x-3, event.y-3, event.x+3, event.y+3, fill="white")
+		else:
+			name = configMenu[1].get(1.0, "end-1c")
+			col = "#000000"
+			if name in colors:
+				col = colors[name]
+			else:
+				col = gethex()
+				colors[name] = col
+			rects.append(canv.create_rectangle(prevclick[0], prevclick[1], event.x, event.y, fill=col))
+			data.append([min(prevclick[0], event.x), min(prevclick[1], event.y),
+				max(prevclick[0], event.x) - min(prevclick[0], event.x), max(prevclick[1], event.y) - min(prevclick[1], event.y),
+				configMenu[1].get(1.0, "end-1c")])
+			prevclick[0] = False
+			canv.delete(prevclick[2])
+	elif currentTool[0] == "deletewall":
+		for i in range(0, len(data)):
+			if 0 < event.x - data[i][0] < data[i][2] and 0 < event.y - data[i][1] < data[i][3]:
+				canv.delete(rects[i])
+				data.pop(i)
+				rects.pop(i)
+				break
 
 def showData():
 	messagebox.showinfo("Boxes", repr(data))
